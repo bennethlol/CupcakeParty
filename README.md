54bac03cccd9a909126e099cf85db827e4334c06
bennethlol <2006K.Hudson@gmail.com>
2024-06-12 13:49:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CupcakeParty
+
+A program that will calculate the number of cupcakes left over in a class of 28.
+
+## Resources
+
+https://www.cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2022/ccc/juniorEF.pdf
---
+++ src/com/kenneth/CupcakeParty.java
@@ -0,0 +1,54 @@
+package com.kenneth;
+
+import java.util.Scanner;
+
+/**
+ * @author Kenneth Hudson
+ * 02/09/2024
+ * A program that will calculate the number of cupcakes left over in a class of 28
+ */
+
+public class CupcakeParty {
+	
+	/**
+	 * Main Method
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		
+		int classStudents = 28;
+		int reg = 0;
+		int sml = 0;
+		
+		Scanner input = new Scanner(System.in);
+		
+		// Takes user input of the total of regular boxes
+		System.out.println("Enter the amount of regular boxes");
+		System.out.println("> ");
+		reg = input.nextInt();
+		
+		// Takes user input of the total of small boxes
+		System.out.println("Enter the amount of small boxes");
+		System.out.println("> ");
+		sml = input.nextInt();
+		
+		// Calculate total amount of cupcakes and subtract the amount by the number of students 
+		int totalCupcakes = reg * 8 + sml * 3;
+		int cupcakesRemain = totalCupcakes - classStudents;
+		
+		
+		// Returns the total cupcakes remaining
+		String cupcakeMessage = calculateCupcakes(cupcakesRemain);
+		System.out.println(cupcakeMessage);	
+	}
+	
+	/**
+	 * Creates a greeting using a custom greeting and name
+	 * @param cupcakesRemain Calculates the total amount of cupcakes remaining
+	 * @return The total amount of remaining cupcakes
+	 */
+	public static String calculateCupcakes(int cupcakesRemain) {
+		return "There will be a total of " + cupcakesRemain + " cupcakes remaining.";
+	}
+
+}
